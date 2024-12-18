f2bd333137471168a14247d932f9ed24668fa8a3
EAD - Ade Putra Nisman Jaya Gea <v.ade.gea@adira.co.id>
2024-12-18 21:39:43 +0700

    proyek kedua

---
+++ script.js
@@ -0,0 +1,9 @@
+// Tangkap tombol dengan ID "contactButton"
+const contactButton = document.getElementById("contactButton");
+
+// Tambahkan event listener untuk tombol
+contactButton.addEventListener("click", function () {
+  alert(
+    "Terima kasih telah menghubungi kami! Kami akan segera merespons pesan Anda."
+  );
+});
